07c34ce912bbc33c96d31cf1caa3e9ee8d4bb1c4
Tari Attila <tari.attila@petrik.hu>
2024-02-03 07:33:38 +0100

    Add project files.

---
+++ DolgozatProject/Dolgozat.cs
@@ -0,0 +1,141 @@
+﻿namespace DolgozatProject
+{
+
+    public class Dolgozat
+    {
+        private List<int> pontok;
+
+        public Dolgozat()
+        {
+            pontok = new List<int>();
+        }
+        public void PontFelvesz(int x)
+        {
+            if (x < -1 || x > 100)
+            {
+                throw new ArgumentException("Érvénytelen paraméter. A pontszámnak -1 és 100 között kell lennie.");
+            }
+
+            pontok.Add(x);
+        }
+        public List<int> Pontok
+        {
+            get { return pontok; }
+        }
+
+        public bool MindenkiMegirta()
+        {
+            return pontok.Any(pont => pont == -1);
+        }
+
+        public int Bukas
+        {
+            get
+            {
+                int count = 0;
+                foreach (int pont in pontok)
+                {
+                    if (pont >= 0 && pont < 50)
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            }
+        }
+
+        public int Elegseges
+        {
+            get
+            {
+                int count = 0;
+                foreach (int pont in pontok)
+                {
+                    if (pont >= 50 && pont < 60)
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            }
+        }
+
+        public int Kozepes
+        {
+            get
+            {
+                int count = 0;
+                foreach (int pont in pontok)
+                {
+                    if (pont >= 61 && pont < 70)
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            }
+        }
+
+        public int Jo
+        {
+            get
+            {
+                int count = 0;
+                foreach (int pont in pontok)
+                {
+                    if (pont >= 71 && pont < 80)
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            }
+        }
+
+        public int Jeles
+        {
+            get
+            {
+                int count = 0;
+                foreach (int pont in pontok)
+                {
+                    if (pont >= 80)
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            }
+        }
+
+        public bool Gyanus(int kivalok)
+        {
+            if (kivalok < 0)
+            {
+                throw new ArgumentException("Érvénytelen paraméter. A kiváló tanulók számának nem lehet negatív.");
+            }
+
+            int jelesek = pontok.Count(pont => pont > 80);
+            return jelesek > kivalok;
+        }
+
+        public bool Ervenytelen
+        {
+            get
+            {
+                int count = 0;
+                foreach (int pont in pontok)
+                {
+                    if (pont == -1)
+                    {
+                        count++;
+                    }
+                }
+
+                return count >= pontok.Count / 2;
+            }
+        }
+    }
+}
+
+
---
+++ TestDolgozatProject/UnitTest1.cs
@@ -0,0 +1,125 @@
+using DolgozatProject;
+namespace TestDolgozatProject
+
+{
+    public class DolgozatTest
+    {
+        [Test]
+        public void PontFelvesz()
+        {
+            Dolgozat dolgozat = new Dolgozat();
+            dolgozat.PontFelvesz(80);
+
+            Assert.AreEqual(1, dolgozat.Pontok.Count);
+            Assert.AreEqual(80, dolgozat.Pontok[0]);
+        }
+        [Test]
+        public void PontFelvesz_Hibas()
+        {
+            Dolgozat dolgozat = new Dolgozat();
+            
+            // Asserting that ArgumentException is thrown for invalid input
+            Assert.Throws<ArgumentException>(() => dolgozat.PontFelvesz(-2));
+            Assert.Throws<ArgumentException>(() => dolgozat.PontFelvesz(101));
+        }
+        [Test]
+        public void MindenkiMegirta_hibas()
+        {
+            Dolgozat dolgozat = new Dolgozat();
+            dolgozat.PontFelvesz(75);
+            dolgozat.PontFelvesz(-1); // Diák nem írta meg a dolgozatot
+
+            Assert.IsTrue(dolgozat.MindenkiMegirta());
+        }
+
+        [Test]
+        public void MindenkiMegirta()
+        {
+            Dolgozat dolgozat = new Dolgozat();
+            dolgozat.PontFelvesz(85);
+            dolgozat.PontFelvesz(70);
+
+            Assert.IsFalse(dolgozat.MindenkiMegirta());
+        }
+        [Test]
+        public void Bukas()
+        {
+            Dolgozat dolgozat = new Dolgozat();
+            dolgozat.PontFelvesz(60);
+            dolgozat.PontFelvesz(40);
+
+            Assert.AreEqual(1, dolgozat.Bukas);
+        }
+
+        [Test]
+        public void Elegseges()
+        {
+            Dolgozat dolgozat = new Dolgozat();
+            dolgozat.PontFelvesz(55);
+            dolgozat.PontFelvesz(58);
+
+            Assert.AreEqual(2, dolgozat.Elegseges);
+        }
+
+        [Test]
+        public void Kozepes()
+        {
+            Dolgozat dolgozat = new Dolgozat();
+            dolgozat.PontFelvesz(63);
+            dolgozat.PontFelvesz(67);
+
+            Assert.AreEqual(2, dolgozat.Kozepes);
+        }
+
+        [Test]
+        public void Jo()
+        {
+            Dolgozat dolgozat = new Dolgozat();
+            dolgozat.PontFelvesz(73);
+            dolgozat.PontFelvesz(78);
+
+            Assert.AreEqual(2, dolgozat.Jo);
+        }
+
+        [Test]
+        public void Jeles()
+        {
+            Dolgozat dolgozat = new Dolgozat();
+            dolgozat.PontFelvesz(85);
+            dolgozat.PontFelvesz(90);
+
+            Assert.AreEqual(2, dolgozat.Jeles);
+        }
+        [Test]
+        public void Ervenytelen()
+        {
+            Dolgozat dolgozat = new Dolgozat();
+            dolgozat.PontFelvesz(85);
+            dolgozat.PontFelvesz(90);
+            dolgozat.PontFelvesz(-1);
+            dolgozat.PontFelvesz(-1);
+            dolgozat.PontFelvesz(-1);
+            dolgozat.PontFelvesz(45);
+            Assert.IsTrue(dolgozat.Ervenytelen);
+            
+        }
+        [Test]
+        public void GyanusNegativ()
+        {
+            Dolgozat dolgozat = new Dolgozat();
+            Assert.Throws<ArgumentException>(() => dolgozat.Gyanus(-1));
+        }
+        [Test]
+        public void Gyanus()
+        {
+            Dolgozat dolgozat = new Dolgozat();
+            dolgozat.PontFelvesz(85);
+            dolgozat.PontFelvesz(90);
+            dolgozat.PontFelvesz(85);
+            dolgozat.PontFelvesz(90);
+            Assert.IsFalse(dolgozat.Gyanus(6));
+        }
+    }
+    
+
+}
